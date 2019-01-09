1402d53231e0f61dc53457750795851d2a215a72
Intekin <kek16e@hotmail.com>
2019-01-09 13:47:59 +0100

    Basic Classes for Save/Load

---
+++ GOL_2019/SaveGame.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GOL_2019
+{
+    class SaveGame
+    {
+        public void Save(List<int[,]> gameToSave)
+        {
+            foreach(int[,] array in gameToSave){
+                string temp ="";
+                for(int y= 0; y < 8; y++)
+                    for(int x = 0; x < 8; x++)
+                    {
+                        temp += array[x, y];
+                    }
+                temp += ",";
+            }
+            //Send to Database_CL
+        }
+    }
+}
