28f48aef9be7751c1317ea614dc807743de41d1f
Intekin <kek16e@hotmail.com>
2019-01-09 15:32:31 +0100

    Test dataGrid

--- GOL_2019/Form1.cs
+++ GOL_2019/Form1.cs
@@ -10,20 +10,43 @@ using System.Windows.Forms;
 
 namespace GOL_2019
 {
-  public partial class Form1 : Form
-  {
-
-
-    public Form1()
-    {
-        InitializeComponent();
-    }
-
-    private void btn_StartNewGame_Click(object sender, EventArgs e)
+    public partial class Form1 : Form
     {
 
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            GameLogic gl = new GameLogic(8,10);
+
+
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.RowTemplate.Height = 25;
+            for (int i = 1; i <= 8; i++)
+            {
+                dataGridView1.Columns.Add("col" + i, "column " + i);
+                dataGridView1.Columns[i - 1].Width = 25;
+            }
+            for (int j = 0; j < 8; j++)
+            {
+                dataGridView1.Rows.Add();
+            }
+
+
+            for(int i = 0; i < 8;i++)
+                for(int j = 0; j < 8; j++)
+                {
+                    if (gl.GameGrid[i, j] == 1)
+                        dataGridView1[i, j].Style.BackColor = Color.Black;
+                }
+        }
+
+        private void btn_StartNewGame_Click(object sender, EventArgs e)
+        {
+
+        }
     }
-  }
 }
-    
-    
+
+
