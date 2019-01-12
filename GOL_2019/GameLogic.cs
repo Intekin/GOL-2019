37ae379fa3c6b2c1b18e24eabe1288c958b2df74
Christoffer <kek16e@hotmail.com>
2019-01-12 10:58:41 +0100

    Fixed GL
    I broke it in the last master merge

--- GOL_2019/GameLogic.cs
+++ GOL_2019/GameLogic.cs
@@ -44,70 +44,71 @@ namespace GOL_2019
         }
 
 
-    // Check for amount of neighbours
-    private int CellHasNeighbours(int[,] grid, int x, int y)
-    {
-      int neighboursCount = 0;
+        // Check for amount of neighbours
+        private int CellHasNeighbours(int[,] grid, int x, int y)
+        {
+            int neighboursCount = 0;
 
-      // Top neighbours; we don't check these for cells on the top row.
-      if (y != 0)
-      {
-        if (x != 0 && grid[x - 1, y - 1] > 0) neighboursCount++;              // Top-left
-        if (grid[x, y - 1] > 0) neighboursCount++;                            // Top-center
-        if (x != grid.GetLength(0) - 1 && grid[x + 1, y - 1] > 0) neighboursCount++;   // Top-right
-      }
+            // Top neighbours; we don't check these for cells on the top row.
+            if (y != 0)
+            {
+                if (x != 0 && grid[x - 1, y - 1] > 0) neighboursCount++;              // Top-left
+                if (grid[x, y - 1] > 0) neighboursCount++;                            // Top-center
+                if (x != grid.GetLength(0) - 1 && grid[x + 1, y - 1] > 0) neighboursCount++;   // Top-right
+            }
 
-      // Left-right neighbours; we don't check for the cells on the very first or last index in a row.
-      if (x != 0 && x != grid.GetLength(0) - 1)
-      {
-        if (grid[x - 1, y] > 0) neighboursCount++; // Left
-        if (grid[x + 1, y] > 0) neighboursCount++; // Right
-      }
+            // Left-right neighbours; we don't check for the cells on the very first or last index in a row.
+            if (x != 0 && x != grid.GetLength(0) - 1)
+            {
+                if (grid[x - 1, y] > 0) neighboursCount++; // Left
+                if (grid[x + 1, y] > 0) neighboursCount++; // Right
+            }
 
-      // Bottom neighbours; not checking for cells on the last row.
-      if (y != grid.GetLength(1) - 1)
-      {
-        if (x != 0 && grid[x - 1, y + 1] > 0) neighboursCount++;              // Bottom-left
-        if (grid[x, y + 1] > 0) neighboursCount++;                            // Bottom-center
-        if (x != grid.GetLength(0) - 1 && grid[x + 1, y + 1] > 0) neighboursCount++;   // Bottom-right
-      }
+            // Bottom neighbours; not checking for cells on the last row.
+            if (y != grid.GetLength(1) - 1)
+            {
+                if (x != 0 && grid[x - 1, y + 1] > 0) neighboursCount++;              // Bottom-left
+                if (grid[x, y + 1] > 0) neighboursCount++;                            // Bottom-center
+                if (x != grid.GetLength(0) - 1 && grid[x + 1, y + 1] > 0) neighboursCount++;   // Bottom-right
+            }
 
-      return neighboursCount;
-    }
-
-
-    // Save current generation, calculate the next.
-    public void Iterate()
-    {
-
-      int cellNeighbours;
-      int[,] newGeneration = (int[,])GameGrid.Clone();
-
-        // Save current generation, calculate the next.
-        
-          // Check the (up to) 8 immediately surrounding cells
-          cellNeighbours = CellHasNeighbours(newGeneration, x, y);
-
-          // Less than 2; die of loneliness, greater than 3; die of overpopulation.
-          if (cellNeighbours < 2 || cellNeighbours > 3)
-          {
-            newGeneration[x, y] = 0;
-            PopulatedCells--;
-          }
-
-          // Empty cell with 3; now a not-so-empty cell.
-          if (newGeneration[x, y] == 0 && cellNeighbours == 3)
-          {
-            newGeneration[x, y] = 1;
-            PopulatedCells++;
-          }
+            return neighboursCount;
         }
 
-      GameGrid = newGeneration;
-      Generations.Add(newGeneration);  // Save current generation before the next iteration
-      // Används medans UI-delen inte är färdig. Project Settings -> Output type = Console
-      //PrintToConsole(false);
-    }
+
+        // Save current generation, calculate the next.
+        public void Iterate()
+        {
+
+            int cellNeighbours;
+            int[,] newGeneration = (int[,])GameGrid.Clone();
+
+            for (int y = 0; y < GridSize; y++)
+                for (int x = 0; x < GridSize; x++)
+                {
+                    // Check the (up to) 8 immediately surrounding cells
+                    cellNeighbours = CellHasNeighbours(newGeneration, x, y);
+
+                    // Less than 2; die of loneliness, greater than 3; die of overpopulation.
+                    if (cellNeighbours < 2 || cellNeighbours > 3)
+                    {
+                        newGeneration[x, y] = 0;
+                        PopulatedCells--;
+                    }
+
+                    // Empty cell with 3; now a not-so-empty cell.
+                    if (newGeneration[x, y] == 0 && cellNeighbours == 3)
+                    {
+                        newGeneration[x, y] = 1;
+                        PopulatedCells++;
+                    }
+                }
+
+            GameGrid = newGeneration;
+            Generations.Add(newGeneration);  // Save current generation before the next iteration
+                                             // Används medans UI-delen inte är färdig. Project Settings -> Output type = Console
+                                             //PrintToConsole(false);
+        }
 
 
         // Alternate form of displaying output. Project Settings -> Output type = Console will open a console window alongside the WinForm
